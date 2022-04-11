d7c667aa413bccd9999ff7635a6e85856079be56
Angel Rodriguez <angelrodri@hotmail.com>
2022-04-11 15:37:32 -0700

    updated getCategories to take count/offset args and wrote getClueHtml func to calculate grid-row-start based on the input val

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-async function getCategories() {
-    let response = await fetch('https://jservice.io/api/categories?count=4&offset=30');
+async function getCategories(count, offset) {
+    let response = await fetch(`https://jservice.io/api/categories?count=${count}&offset=${offset}`);
     let data = await response.json();
     return data;
+}
+
+function getClueHtml(clueValue) {
+    return `<div class="my-category-clue" style="grid-row-start: ${clueValue / 100 + 1}">$${clueValue}</div>`
 }
 
 function getCategoryHml(category) {
     return  `
         <div class="my-category-title">${category.title}</div>
-        <div class="my-category-clue" style="grid-row-start:2">$100</div>
-        <div class="my-category-clue" style="grid-row-start:3">$200</div>
-        <div class="my-category-clue" style="grid-row-start:4">$300</div>
-        <div class="my-category-clue" style="grid-row-start:5">$400</div>
+        ${getClueHtml(100)}
+        ${getClueHtml(200)}
+        ${getClueHtml(300)}
+        ${getClueHtml(400)}
     `
 }
 
-getCategories().then(categories => {
+getCategories(5).then(categories => {
     console.log(categories);
     document.body.innerHTML = `
         <div class="board">
